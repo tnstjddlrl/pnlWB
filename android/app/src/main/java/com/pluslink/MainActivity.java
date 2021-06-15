9ea62044bfe165fb7f8cb967f9da6f6adde23f4f
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2021-06-15 09:50:21 +0900

    패키지명 변경 및 릴리즈 빌딩 하고 빌드했는데 엑세스 키가 없다

---
+++ android/app/src/main/java/com/pluslink/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pluslink.pluslinkapp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "pnl";
+  }
+}
